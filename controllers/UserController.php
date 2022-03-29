5e51879a3128fc318639a0708ce8df30c2649ed9
Eliel Siqueira <elielkaragua2016@gmail.com>
2022-03-29 17:38:44 -0300

    Finalizeijons

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -7,13 +7,30 @@
             $user = new User(null, $name,$email,$pass);
             $user->create();
         }elseif ($route[1] == 'delete') {
-            $id =$_POST['id'];
+            $id = $_POST['id'];
             $user = new User($id, null,null,null);
             $user->delete();
+        }
+        elseif ($route[1] == 'update') {
+            $id = $_POST['id'];
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $pass = $_POST['pass'];
+            $user = new User($id,$name,$email,$pass);
+            $user->update();
+        }
+        elseif ($route[1] == 'select-all') {
+            $user = new User(null,null,null,null);
+            $user->selectAll();
+        }
+        elseif ($route[1] == 'select-by-id') {
+            $id = $_POST['id'];
+            $user = new User($id,null,null,null);
+            $user->selectByid();
         }else{
             echo "Página não econtrada";
         }
     }else{
         echo "Página não econtrada";
-    }
+}
 ?>
--- models/User.php
+++ models/User.php
@@ -38,5 +38,47 @@ class User{
                 echo "Error : " . $e->getMessage();
             }
     }
+    function update(){
+        $db = new Database(); 
+        try{
+            $stmt = $db->conn->prepare("UPDATE users SET  name = :name, email = :email, pass = :pass WHERE id = :id;");
+            $stmt->bindParam(':id', $this->id);
+            $stmt->bindParam(':name', $this->name);
+            $stmt->bindParam(':email', $this->email);
+            $stmt->bindParam(':pass', $this->pass);
+            $stmt->execute();
+            echo "Modificado com sucesso";
+        }
+        catch(PDOException $e){
+            echo "Error : " . $e->getMessage();
+        }   
+    }
+    function selectAll(){
+        $db = new Database(); 
+        try{
+            $stmt = $db->conn->prepare("SELECT * FROM users");
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            print_r($result);
+            echo "Selecionado com sucesso";
+        }
+        catch(PDOException $e){
+            echo "Error : " . $e->getMessage();
+        }   
+    }
+    function selectByid(){
+        $db = new Database(); 
+        try{
+            $stmt = $db->conn->prepare("SELECT * FROM users WHERE id = :id;");
+            $stmt->bindParam(':id', $this->id);
+            $stmt->execute();
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            print_r($result);
+            echo "Selecionado o usuário com sucesso";
+        }
+        catch(PDOException $e){
+            echo "Error : " . $e->getMessage();
+        }   
+    }
 }
 ?>
